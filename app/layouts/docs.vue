258478778a9f3f348191f116bcc14cc35b744cd8
Matthew Maennche <matthew@maennche.com>
2025-01-06 11:05:48 -0600

    Update docs.vue

--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -1,18 +1,6 @@
-<script setup lang="ts">
-import type { NavItem } from '@nuxt/content'
-
-const navigation = inject<Ref<NavItem[]>>('navigation')
-</script>
-
 <template>
   <UContainer>
     <UPage>
-      <template #left>
-        <UAside>
-          <UNavigationTree :links="mapContentNavigation(navigation)" />
-        </UAside>
-      </template>
-
       <slot />
     </UPage>
   </UContainer>
